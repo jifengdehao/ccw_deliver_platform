2aabb0f0388414fde49b8ef4eb4d5bb0cf2f2158
wufengliang <wufengliang@ccw163.com>
2017-11-17 18:56:32 +0800

    修改打包编译相关错误

--- config/config.js
+++ config/config.js
@@ -4,9 +4,8 @@
  * email: zhangwenlong@ccw163.com
  * 功能模块:服务器api地址
  */
-let config = {
-  // apiDomain:'/api',
-  apiDomain: 'http://192.168.0.158:8094',
+var config = {
+  apiDomain: 'http://192.168.0.4:8094',
   imgUpload: ''
 }
 export default config
