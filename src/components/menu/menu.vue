ea5bc7bba59404ff8e1ad7e16dbbd081dafbb923
zhangwenlong <zhangwenlong@ccw163.com>
2017-12-01 17:03:08 +0800

    17:03 修复bug

--- src/components/menu/menu.vue
+++ src/components/menu/menu.vue
@@ -15,60 +15,62 @@
         </template>
       </Submenu>
     </template>
-    <!--<MenuItem name="home">-->
-      <!--<Icon type="home" :size="iconSize"></Icon>-->
-      <!--<span class="layout-text">首页</span>-->
-    <!--</MenuItem>-->
-    <!--<MenuItem name="order">-->
-      <!--<Icon type="ios-paper" :size="iconSize"></Icon>-->
-      <!--<span class="layout-text">订单管理</span>-->
-    <!--</MenuItem>-->
-    <!--<Submenu name="">-->
-      <!--<template slot="title">-->
-        <!--<Icon type="ios-people" :size="iconSize"></Icon>-->
-        <!--<span class="layout-text">骑士管理</span>-->
-      <!--</template>-->
-      <!--<MenuItem name="cUncommitted">未提交资料</MenuItem>-->
-      <!--<MenuItem name="cAuditing">骑士审核</MenuItem>-->
-      <!--<MenuItem name="cTrain">培训管理</MenuItem>-->
-      <!--<MenuItem name="cPersonal">人员管理</MenuItem>-->
-    <!--</Submenu>-->
-    <!--<MenuItem name="configuration">-->
-      <!--<Icon type="ios-gear" :size="iconSize"></Icon>-->
-      <!--<span class="layout-text">配置设置</span>-->
-    <!--</MenuItem>-->
-    <!--<Submenu name="">-->
-      <!--<template slot="title">-->
-        <!--<Icon type="stats-bars" :size="iconSize"></Icon>-->
-        <!--<span class="layout-text">数据管理</span>-->
-      <!--</template>-->
-      <!--<MenuItem name="dataCenter">数据中心</MenuItem>-->
-      <!--<MenuItem name="dataDeliver">配送员数据</MenuItem>-->
-    <!--</Submenu>-->
-    <!--<Submenu name="">-->
-      <!--<template slot="title">-->
-        <!--<Icon type="settings" :size="iconSize"></Icon>-->
-        <!--<span class="layout-text">设置中心</span>-->
-      <!--</template>-->
-      <!--<MenuItem name="s_platform">平台用户</MenuItem>-->
-      <!--<MenuItem name="s_rbac">权限管理</MenuItem>-->
-      <!--<MenuItem name="s_rolelist">角色列表</MenuItem>-->
-      <!--<MenuItem name="s_operation">操作日志</MenuItem>-->
-      <!--<MenuItem name="s_message">消息管理</MenuItem>-->
-      <!--<MenuItem name="setting_message">消息</MenuItem>-->
-    <!--</Submenu>-->
-    <!--<Submenu name="">-->
-      <!--<template slot="title">-->
-        <!--<Icon type="person-stalker" :size="iconSize"></Icon>-->
-        <!--<span class="layout-text">分销管理</span>-->
-      <!--</template>-->
-      <!--<MenuItem name="d_extend">推广列表</MenuItem>-->
-      <!--<MenuItem name="d_list">分销列表</MenuItem>-->
-    <!--</Submenu>-->
-    <!--<MenuItem name="finance">-->
-      <!--<Icon type="person-stalker" :size="iconSize"></Icon>-->
-      <!--<span class="layout-text">财务对账</span>-->
-    <!--</MenuItem>-->
+    <!--
+    <MenuItem name="home">
+      <Icon type="home" :size="iconSize"></Icon>
+      <span class="layout-text">首页</span>
+    </MenuItem>
+    <MenuItem name="order">
+      <Icon type="ios-paper" :size="iconSize"></Icon>
+      <span class="layout-text">订单管理</span>
+    </MenuItem>
+    <Submenu name="">
+      <template slot="title">
+        <Icon type="ios-people" :size="iconSize"></Icon>
+        <span class="layout-text">骑士管理</span>
+      </template>
+      <MenuItem name="cUncommitted">未提交资料</MenuItem>
+      <MenuItem name="cAuditing">骑士审核</MenuItem>
+      <MenuItem name="cTrain">培训管理</MenuItem>
+      <MenuItem name="cPersonal">人员管理</MenuItem>
+    </Submenu>
+    <MenuItem name="configuration">
+      <Icon type="ios-gear" :size="iconSize"></Icon>
+      <span class="layout-text">配置设置</span>
+    </MenuItem>
+    <Submenu name="">
+      <template slot="title">
+        <Icon type="stats-bars" :size="iconSize"></Icon>
+        <span class="layout-text">数据管理</span>
+      </template>
+      <MenuItem name="dataCenter">数据中心</MenuItem>
+      <MenuItem name="dataDeliver">配送员数据</MenuItem>
+    </Submenu>
+    <Submenu name="">
+      <template slot="title">
+        <Icon type="settings" :size="iconSize"></Icon>
+        <span class="layout-text">设置中心</span>
+      </template>
+      <MenuItem name="s_platform">平台用户</MenuItem>
+      <MenuItem name="s_rbac">权限管理</MenuItem>
+      <MenuItem name="s_rolelist">角色列表</MenuItem>
+      <MenuItem name="s_operation">操作日志</MenuItem>
+      <MenuItem name="s_message">消息管理</MenuItem>
+      <MenuItem name="setting_message">消息</MenuItem>
+    </Submenu>
+    <Submenu name="">
+      <template slot="title">
+        <Icon type="person-stalker" :size="iconSize"></Icon>
+        <span class="layout-text">分销管理</span>
+      </template>
+      <MenuItem name="d_extend">推广列表</MenuItem>
+      <MenuItem name="d_list">分销列表</MenuItem>
+    </Submenu>
+    <MenuItem name="finance">
+      <Icon type="person-stalker" :size="iconSize"></Icon>
+      <span class="layout-text">财务对账</span>
+    </MenuItem>
+    -->
   </Menu>
 </template>
 <script type='text/ecmascript-6'>
@@ -99,7 +101,6 @@
     },
     methods: {
       route(name) {
-        console.log(name)
         this.$router.push('/' + name)
       },
       getMenu() {
