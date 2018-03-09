4f6f02f588f2f96f248522c3fbef38d43b7b3e8e
hushangjun <hushangjun@caichengwang.com>
2018-03-09 17:24:14 +0800

    解决oss文件不能打包的问题

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -37,7 +37,15 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
+        include: [
+          resolve('src'), 
+          resolve('test'),
+          resolve('/node_modules/ali-oss/'),
+          resolve('/node_modules/co-gather/')
+        ],
+        query: {
+          presets: ['es2015']
+        }
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
